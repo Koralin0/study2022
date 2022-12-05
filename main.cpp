5f43f1c5da647f9118b2bb09fe6c820d8ffce2ea
Alexadnra Balinskaiya <alexandrabalinskaiya@gmail.com>
2022-12-05 21:38:41 +0200

    add powto main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
     float M, T;
-    cout << "Enter the weight (kg): " << endl;
     cin >> M;
     T = M / 1000;
     cout << "Your weight (tons): " << (int)T << endl;
+    cout << pow(T,M)
 }
