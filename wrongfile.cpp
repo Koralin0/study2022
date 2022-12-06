620ffd26b6355f75f545964c9fcff02bdf2670b4
Alexadnra Balinskaiya <alexandrabalinskaiya@gmail.com>
2022-12-06 10:21:41 +0200

    reset commit

---
+++ wrongfile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include "math.h" 
+
+int main()
+{
+    float M, T;
+    cin >> M;
+    T = M / 1000;
+    cout << "Your weight (tons): " << (int)T << endl;
+    cout << pow(T,M)
+}
